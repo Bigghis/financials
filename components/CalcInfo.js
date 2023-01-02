e80c12944757abdea09667979a33290cbd62bafa
Bigghis <pa.casciano@gmail.com>
2023-01-02 22:11:10 +0100

    adds alternative calcss

--- components/CalcInfo.js
+++ components/CalcInfo.js
@@ -8,25 +8,27 @@ import texts from '../info/texts.json';
 
 import styles from '../styles/CalcInfo.module.css';
 import homeStyles from '../styles/Home.module.css';
-import { toPercent } from '../logic/utils';
+import { toDecimal, toPercent } from '../logic/utils';
 
-function getClsColor(value) {
+
+const RANGES = [1,2];
+const RANGES_WITH_DIVIDENDS = [1.5, 2];
+
+function getClsColor(value, ranges) {
     let _cls = homeStyles.bold;
     if (value) {
-        if (value < 1) {
+        if (value < ranges[0]) {
             _cls = `${homeStyles.bold} ${homeStyles.red}`;
         }
-        else if (value >= 1 && value < 2) {
+        else if (value >= ranges[0] && value < ranges[1]) {
             _cls = `${homeStyles.bold} ${homeStyles.yellow}`;
         }
-        else if (value >= 2) {
+        else if (value >= ranges[1]) {
             _cls = `${homeStyles.bold} ${homeStyles.green}`;
         }
     }
     return _cls
 }
-
-// const infoIcon = tooltip ? <span data-tip={tooltip}><InfoIcon /></span> : null;
 
 function CalcInfo({ data }) {
     const dataContext = useContext(DataContext);
@@ -40,14 +42,27 @@ function CalcInfo({ data }) {
 
     const dividendYield = commonData && lastDividend && commonData.price 
     && commonData.price.regularMarketPrice 
-    ? toPercent((lastDividend) / commonData.price.regularMarketPrice)
+    ? toPercent((lastDividend / commonData.price.regularMarketPrice), 2)
     : null
 
-    const { averages: { averageNetIncomeGrowthValue }} = data
 
+    
+    
+    const { averages: { averageNetIncomeGrowthValue }} = data
+    
     const netIncomeGrowthPeRatio = (averageNetIncomeGrowthValue * 100) / trailingPe
     const netIncomeGrowthPeFwdRatio = (averageNetIncomeGrowthValue * 100) / forwardPe
 
+    const netIncomeGrowthPeRatioWithDividends = dividendYield 
+    ? ((averageNetIncomeGrowthValue + toDecimal(dividendYield)) / trailingPe) * 100
+    : null
+
+    const netIncomeGrowthPeFwddRatioWithDividends = dividendYield 
+    ? ((averageNetIncomeGrowthValue + toDecimal(dividendYield)) / forwardPe) * 100
+    : null
+
+
+    
     if (data && commonData && commonData.summaryDetail) {
         return (<div>
                     <h4 className={styles.title}>
@@ -55,19 +70,29 @@ function CalcInfo({ data }) {
                     </h4>
                     <div>
                         <div className={styles.row}>
+                            <span className={styles.iconTooltip} data-tip={txt.dividendYield.tooltip}><InfoIcon /></span>
+                            <span>Dividend Yield: </span>
+                            <span className={homeStyles.bold}>{dividendYield}</span>
+                        </div>
+                        <div className={styles.row}>
                             <span className={styles.iconTooltip} data-tip={txt.growthRatePeRatio.tooltip}><InfoIcon /></span>
                             <span>Ratio (avgNetIncomeGrowthRate) / (P/E): </span>
-                            <span className={getClsColor(netIncomeGrowthPeRatio)}>{netIncomeGrowthPeRatio.toFixed(2)}</span>
+                            <span className={getClsColor(netIncomeGrowthPeRatio, RANGES)}>{netIncomeGrowthPeRatio.toFixed(2)}</span>
                         </div>
                         <div className={styles.row}>
                             <span className={styles.iconTooltip} data-tip={txt.growthRatePeRatioFwd.tooltip}><InfoIcon /></span>
                             <span>Ratio (avgNetIncomeGrowthRate) / (P/E fwd): </span>
-                            <span className={getClsColor(netIncomeGrowthPeFwdRatio)}>{netIncomeGrowthPeFwdRatio.toFixed(2)}</span>
+                            <span className={getClsColor(netIncomeGrowthPeFwdRatio, RANGES)}>{netIncomeGrowthPeFwdRatio.toFixed(2)}</span>
                         </div>
                         <div className={styles.row}>
-                            <span className={styles.iconTooltip} data-tip={txt.dividendYield.tooltip}><InfoIcon /></span>
-                            <span>Dividend Yield: </span>
-                            <span>{dividendYield}</span>
+                            <span className={styles.iconTooltip} data-tip={txt.growthRatePeRatioDividend.tooltip}><InfoIcon /></span>
+                            <span>Ratio (avgNetIncomeGrowthRate + dividendYield) / (P/E fwd): </span>
+                            <span className={getClsColor(netIncomeGrowthPeRatioWithDividends, RANGES_WITH_DIVIDENDS)}>{netIncomeGrowthPeRatioWithDividends ? netIncomeGrowthPeRatioWithDividends.toFixed(2) : ' - '}</span>
+                        </div>
+                        <div className={styles.row}>
+                            <span className={styles.iconTooltip} data-tip={txt.growthRatePeFwdRatioDividend.tooltip}><InfoIcon /></span>
+                            <span>Ratio (avgNetIncomeGrowthRate + dividendYield) / (P/E fwd): </span>
+                            <span className={getClsColor(netIncomeGrowthPeFwddRatioWithDividends, RANGES_WITH_DIVIDENDS)}>{netIncomeGrowthPeFwddRatioWithDividends ? netIncomeGrowthPeFwddRatioWithDividends.toFixed(2) : ' - '}</span>
                         </div>
                     </div>
                     <ReactTooltip effect="solid" className={homeStyles.tooltipCustom} />
