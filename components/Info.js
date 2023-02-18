82d1e87d9e4d2063416b3fa88783e98e58d14e15
Bigghis <pa.casciano@gmail.com>
2023-02-18 20:54:53 +0100

    fix missing useContext

--- components/Info.js
+++ components/Info.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { SpinnerDotted } from 'spinners-react';
 import Table from './Table';
 import NoData from './NoData';
